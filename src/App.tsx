0808a2fb78ce4d65f15a138f92211923c7a64d6b
paullarasales <ahmadzufukli@gmail.com>
2024-11-03 10:40:51 +0800

    clean project

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,8 @@ import Layout from './components/Layout';
 
 const App: React.FC = () => {
     return (
-        <Layout>
-            <div>
-                <h2 className="text-2xl font-bold mb-4">Welcome to the Dashboard</h2>
-                <p>This is your main content area.</p>
-            </div>
-        </Layout>
+        <>
+        </>
     );
 };
 
